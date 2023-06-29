374b167ad96d760207b80bbed29a1b17cd9572d7
xiaobingch <xiaobingch@gmail.com>
2023-06-29 22:34:03 +0800

    reactive, toRefs定义对象类型的数据

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
    {{ name }}-
-   {{ num }}-
-   {{ arr[1] }}- 
-    {{ obj1.age }}
+   {{ obj }}-
+   {{ age }}- 
+    {{ sex }}
   </div>
 </template>
 
 
 <script>
 
-  import { defineComponent, ref } from 'vue'
+  import { defineComponent, ref, reactive, toRefs} from 'vue'
 
   export default defineComponent({
     name: 'Home',
@@ -19,16 +19,25 @@
    
     },
     setup(props, ctx) {
-      let num = ref(111)
-      let name = ref('jack')
-      let arr = ref([1, 'aa', 'cc'])
-      let obj1 = ref({age:20})
+      // let num = ref(111)
+      // let name = ref('jack')
+      // let arr = ref([1, 'aa', 'cc'])
+      // let obj1 = ref({age:20})
+
+
+      let data = reactive({
+        name: 'jack',
+        age: 20,
+        sex: ['man', 'wuman'],
+        obj: {price:100}
+      })
 
       return {
-        name,
-        num,
-        arr,
-        obj1
+        ...toRefs(data)
+        // name,
+        // num,
+        // arr,
+        // obj1
       }
     } 
   })
